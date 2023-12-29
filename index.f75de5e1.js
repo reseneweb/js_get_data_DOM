75e9a3cceee00c01148ba566b77eb2698c34a4f8
Dmytro Fefelov <reseneweb@gmail.com>
2023-12-29 16:14:52 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+function thousandsSeparator(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+const populations = document.querySelectorAll(".population");
+const total = [
+    ...populations
+].reduce((a, b)=>a + +b.innerText.replaceAll(",", ""), 0);
+document.querySelector(".total-population").textContent = thousandsSeparator(total);
+document.querySelector(".average-population").textContent = thousandsSeparator(total / populations.length);
+
+//# sourceMappingURL=index.f75de5e1.js.map
